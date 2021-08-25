45a4c52163463807bddc3e69c6544e42e91d06b4
WANG Guoqin <wangguoqin1001@gmail.com>
2021-08-25 17:24:17 +0800

    Initial implementation of serial port protocol

--- serial.cpp
+++ serial.cpp
@@ -1,6 +1,37 @@
+#include <QDebug>
+
 #include "serial.h"
 
 serial::serial(QObject *parent) : QObject(parent)
 {
+    this->serialport->setBaudRate(SERIAL_BAUDRATE);
+    this->serialport->setDataBits(SERIAL_DATABITS);
+    this->serialport->setParity(SERIAL_PARITY);
+    this->serialport->setStopBits(SERIAL_STOPBITS);
+    this->serialport->setFlowControl(SERIAL_FLOWCONTROL);
 
+    if(serialport->open(QIODevice::ReadWrite)) {
+        qDebug() << "Serial port opened.";
+    } else {
+        qDebug() << "Serial port open failed" << serialport->error();
+    }
+
+    connect(this->serialport, &QSerialPort::readyRead, this, &serial::readData);
+}
+
+serial::~serial()
+{
+    if(this->serialport->isOpen())
+        this->serialport->close();
+    qDebug() << "Serial port closed.";
+}
+
+void serial::readData()
+{
+    QByteArray data = serialport->readAll();
+}
+
+void serial::writeData(const QByteArray &data)
+{
+    serialport->write(data);
 }
--- serial.h
+++ serial.h
@@ -4,15 +4,27 @@
 #include <QObject>
 #include <QSerialPort>
 
+#define SERIAL_BAUDRATE QSerialPort::Baud115200
+#define SERIAL_DATABITS QSerialPort::Data8
+#define SERIAL_PARITY QSerialPort::NoParity
+#define SERIAL_STOPBITS QSerialPort::OneStop
+#define SERIAL_FLOWCONTROL QSerialPort::NoFlowControl
+
 class serial : public QObject
 {
     Q_OBJECT
 public:
     explicit serial(QObject *parent = nullptr);
+    ~serial();
+
+private:
+    QSerialPort *serialport = new QSerialPort(this);
 
 signals:
 
 public slots:
+    void readData();
+    void writeData(const QByteArray &data);
 };
 
 #endif // SERIAL_H
