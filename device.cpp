91ba2c6fcc34e158f5e5930a2515880ecfe40209
WANG Guoqin <wangguoqin1001@gmail.com>
2021-09-07 14:03:50 +0800

    Copying contents from messages to devices

--- device.cpp
+++ device.cpp
@@ -23,19 +23,56 @@ devAmp::devAmp(QObject *parent) : device(parent)
 
 }
 
-void device::updateDevice(const int dev, const QString str)
+void device::updateDevice(const msgFreq m)
 {
     for (device *d : device::deviceList)
     {
-        d->find_and_set(dev, str);
+        d->findAndUpdate(m);
     }
 }
 
-void device::find_and_set(const int dev, const QString str)
+void device::updateDevice(const msgDist m)
 {
-    if (this->dId == dev)
+    for (device *d : device::deviceList)
     {
-        this->str = str;
-        emit this->gotData();
+        d->findAndUpdate(m);
+    }
+}
+
+void device::updateDevice(const msgAmp m)
+{
+    for (device *d : device::deviceList)
+    {
+        d->findAndUpdate(m);
+    }
+}
+
+void device::findAndUpdate(const msgFreq m)
+{
+    if (this->dId == m.device)
+    {
+        devFreq dev = static_cast<devFreq>(this);
+        dev.str = m.origin;
+        emit dev.gotData();
+    }
+}
+
+void device::findAndUpdate(const msgDist m)
+{
+    if (this->dId == m.device)
+    {
+        devDist dev = static_cast<devDist>(this);
+        dev.str = m.origin;
+        emit dev.gotData();
+    }
+}
+
+void device::findAndUpdate(const msgAmp m)
+{
+    if (this->dId == m.device)
+    {
+        devAmp dev = static_cast<devAmp>(this);
+        dev.str = m.origin;
+        emit dev.gotData();
     }
 }
