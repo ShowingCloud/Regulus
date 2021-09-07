c80f1ae022fc3f2db7f9d1e0c62f769c01ca7293
WANG Guoqin <wangguoqin1001@gmail.com>
2021-09-07 14:33:00 +0800

    Try fixing the pointer problem (no checking, no info)

--- device.cpp
+++ device.cpp
@@ -49,30 +49,30 @@ void device::updateDevice(const msgAmp *m)
 
 void device::findAndUpdate(const msgFreq *m)
 {
-    if (this->dId == m.device)
+    if (this->dId == m->device)
     {
         devFreq dev = static_cast<devFreq>(this);
-        dev.str = m.origin;
+        dev.str = m->origin;
         emit dev.gotData();
     }
 }
 
 void device::findAndUpdate(const msgDist *m)
 {
-    if (this->dId == m.device)
+    if (this->dId == m->device)
     {
         devDist dev = static_cast<devDist>(this);
-        dev.str = m.origin;
+        dev.str = m->origin;
         emit dev.gotData();
     }
 }
 
 void device::findAndUpdate(const msgAmp *m)
 {
-    if (this->dId == m.device)
+    if (this->dId == m->device)
     {
         devAmp dev = static_cast<devAmp>(this);
-        dev.str = m.origin;
+        dev.str = m->origin;
         emit dev.gotData();
     }
 }
