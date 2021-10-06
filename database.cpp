a2f880572092bb5926ba3a9018df9f23f16bf93e
WANG Guoqin <wangguoqin1001@gmail.com>
2021-10-06 14:42:48 +0800

    Fixed the no-return problem for db insert funcs

--- database.cpp
+++ database.cpp
@@ -87,47 +87,34 @@ database &operator<< (database &db, const msgFreq &msg)
 
 database &operator<< (database &db, const msgDist &msg)
 {
-
+    return db;
 }
 
 database &operator<< (database &db, const msgAmp &msg)
 {
-
+    return db;
 }
 
 database &operator<< (database &db, const alert &alert)
 {
-
+    return db;
 }
 
 database &operator<< (database &db, const msgCntlAmp &msg)
 {
-
+    return db;
 }
 
 database &operator<< (database &db, const msgCntlFreq &msg)
 {
-
+    return db;
 }
 
 database &operator<< (database &db, const msgCntlDist &msg)
 {
-
+    return db;
 }
 
-//msgFreq &operator<< (msgFreq &m, const QByteArray &data)
-//{
-//    QDataStream(data) >> m.holder8 /* header */ >> m.atten >> m.voltage
-//                      >> m.current >> m.radio_stat >> m.mid_stat >> m.lock_a1
-//                      >> m.lock_a2 >> m.lock_b1 >> m.lock_b2 >> m.ref_10_1
-//                      >> m.ref_2 >> m.ref_10_3 >> m.ref_4 >> m.holder8 /* device */
-//                      >> m.handshake >> m.serialId >> m.masterslave >> m.holder8 /* tailer */;
-//    m.ref_10_2 = m.ref_2 & 0x0F;
-//    m.ref_inner_1 = (m.ref_2 & 0xF0) >> 4;
-//    m.ref_10_4 = m.ref_4 & 0x0F;
-//    m.ref_inner_2 = (m.ref_4 & 0xF0) >> 4;
-//}
-//
 //msgDist &operator<< (msgDist &m, const QByteArray &data)
 //{
 //    if (data.length() != msgUplink::mlen)
